77a6d98e480e52b7acbcb2603ce1e2472a7a6264
Abdelrahman Eid <abdelrahman.eid97@gmail.com>
2024-05-24 22:57:54 +0300

    Add rescale_bboxes function

--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -1,3 +1,15 @@
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
 def outputs_to_objects(outputs, img_size, id2label):
     m = outputs.logits.softmax(-1).max(-1)
     pred_labels = list(m.indices.detach().cpu().numpy())[0]
